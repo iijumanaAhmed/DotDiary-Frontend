8f03549ec81285163cc11c3f7ba04c421b753ac6
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-10-28 13:49:09 +0300

    Add delete functionality to the SessionDetail component

--- src/components/SessionDetail/SessionDetail.jsx
+++ src/components/SessionDetail/SessionDetail.jsx
@@ -16,6 +16,14 @@ function SessionDetail() {
         }
     }, [])
 
+    async function deleteSession() {
+        const response = await axios.delete(`http://127.0.0.1:8000/api/focusLogs/${sessionId}`)
+        
+        if (response.status === 204) {
+            navigate(`/focusLogs`)
+        }
+    }
+
     return (
         <div>
             <h1>Session #{session.id} Detail</h1>
@@ -27,6 +35,7 @@ function SessionDetail() {
             <p>to do list</p>
             <p>tag</p>
             <p>distractions</p>
+            <button onClick={deleteSession}>Delete</button>
         </div>
     )
 }
