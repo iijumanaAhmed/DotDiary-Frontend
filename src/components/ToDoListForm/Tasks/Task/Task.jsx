77d7a2061219e6d5a9e3c646086de8c30b9bda1f
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-11-04 02:44:27 +0300

    Add checkbox to the Task component and update it onChange

--- src/components/ToDoListForm/Tasks/Task/Task.jsx
+++ src/components/ToDoListForm/Tasks/Task/Task.jsx
@@ -9,13 +9,15 @@ function Task({ toDoListId, taskId }) {
         is_done: '',
         priority: ''
     })
-
+    
     const PRIORITY = {
         L: 'Low',
         M: 'Medium',
         H: 'High'
     }
-
+    
+    const [isChecked, setIsChecked] = useState()
+    
     function handleChange(event) {
         setTaskData({ ...taskData, [event.target.name]: event.target.value })
         console.log(taskData)
@@ -24,6 +26,7 @@ function Task({ toDoListId, taskId }) {
     async function displayTask() {
         const response = await authRequest({ method: 'get', url: `http://127.0.0.1:8000/api/toDoLists/${toDoListId}/tasks/${taskId}/` })
         setTaskData(response.data)
+        setIsChecked(response.data.is_done)
     }
     useEffect(() => {
         if (taskId) {
@@ -36,8 +39,22 @@ function Task({ toDoListId, taskId }) {
         const response = await authRequest({ data: taskData, method: 'put', url: `http://127.0.0.1:8000/api/toDoLists/${toDoListId}/tasks/${taskId}/` })
     }
 
+    function handleIsChecked() {
+        setIsChecked(!isChecked)
+        updateTaskStatus()
+    }
+    async function updateTaskStatus() {
+        const updatedTaskData = {
+            ...taskData,
+            is_done: !isChecked
+        }
+        const response = await authRequest({ data: updatedTaskData, method: 'put', url: `http://127.0.0.1:8000/api/toDoLists/${toDoListId}/tasks/${taskId}/` })
+    }
+
     return (
         <div>
+            {/* https://dev.to/collegewap/how-to-work-with-checkboxes-in-react-44bc */}
+            <input type='checkbox' checked={isChecked} onChange={handleIsChecked}></input>
             <form onSubmit={updateTask}>
                 <div>
                     <label htmlFor='task'>task </label>
